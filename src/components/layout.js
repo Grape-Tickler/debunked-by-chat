4f7f8bab9cd3889bcf87184836cd7b3ec6a5c40f
Grape-Tickler <243241895+Grape-Tickler@users.noreply.github.com>
2025-11-14 21:41:33 -0500

    add foopatroopah3

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        Built with ✌️&💜 by <a href="https://www.reddit.com/user/GrapeTickler/" target="_blank" rel="noopener noreferrer" textDecoration="none" >(🍇🤗)</a>, (🔫🐻), & (👃🇬🇷)
+        Made with ✌️&💜 by <a href="https://www.reddit.com/user/GrapeTickler/" target="_blank" rel="noopener noreferrer" textDecoration="none" >(🍇🤗)</a>, <a href="https://www.youtube.com/@foopatroopaH3" target="_blank" rel="noopener noreferrer" textDecoration="none" >(🔫🐻)</a>, & (👃🇬🇷)
         {` `}
         
       </footer>
